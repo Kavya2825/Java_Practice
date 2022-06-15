0dd00e4418824de75dce547a024c73879dc892b3
Kavya2825 <107460583+Kavya2825@users.noreply.github.com>
2022-06-14 23:30:10 -0800

    Add files via upload

---
+++ Square (1).java
@@ -0,0 +1,9 @@
+ class Square
+  {
+  public static void main(String args[])
+    {
+    int a=5,b=3;
+    int value=(int)Math.pow(a,2)+(int)Math.pow(b,2);
+  System.out.println("Sum of square of two numbers="+value);
+}
+  }
